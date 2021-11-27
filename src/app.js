1918263e2e3ce84528b4ab09067f72fded085e73
Luis Felipe <l_felipelizeu@hotmail.com>
2021-11-27 11:38:07 -0300

    Create the sign up route and function

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import * as userController from './controllers/userController.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.post('/signup', userController.signUp);
 
 export default app;
