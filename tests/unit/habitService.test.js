3935cf0bac09c2883535884545523c563a422086
Luis Felipe <l_felipelizeu@hotmail.com>
2021-11-29 03:14:29 -0300

    Create an automated test for the change

--- tests/unit/habitService.test.js
+++ tests/unit/habitService.test.js
@@ -1,10 +1,28 @@
 import '../../src/setup.js';
 import faker from 'faker';
+import bcrypt from 'bcrypt';
 import createHabit from '../factories/habitFactory.js';
 import connection from '../../src/database.js';
+import createUser from '../factories/userFactory.js';
 import * as habitService from '../../src/services/habitService.js';
 
 describe('POST /habits', () => {
+    const user = createUser();
+    const token = faker.datatype.uuid();
+
+    beforeAll(async () => {
+        const hashPassword = bcrypt.hashSync(user.password, 10);
+        const result = await connection.query('INSERT INTO users (name, email, image,password) VALUES ($1, $2, $3, $4) RETURNING *;', [user.name, user.email, user.image, hashPassword]);
+        user.id = result.rows[0].id;
+        await connection.query('INSERT INTO sessions (user_id, token) VALUES ($1, $2);', [user.id, token]);
+    });
+
+    afterAll(async () => {
+        await connection.query('DELETE FROM habits;');
+        await connection.query('DELETE FROM sessions;');
+        await connection.query('DELETE FROM users;');
+    });
+
     it('should returns false for empty days array', () => {
         const habit = {
             name: faker.lorem.words(3),
@@ -50,5 +68,13 @@ describe('POST /habits', () => {
 
         const result = habitService.isHabitValid(habit);
         expect(result).toEqual(true);
+    });
+
+    it('should returns the same object', async () => {
+        const habit = createHabit();
+
+        const result = await habitService.createNewHabit(habit, user.id);
+        delete result.id;
+        expect(result).toEqual(habit);
     });
 });
